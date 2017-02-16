8c29d7c491b23d1b01d203cf8d9f393a8fec9afb
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-16 13:42:22 -0600

    Update example2.16.2.js

--- example2.16.2.js
+++ example2.16.2.js
@@ -1,4 +1,4 @@
-var miCanvas= document.getElementarById("miCanvas");
+var miCanvas= document.getElementById("miCanvas");
 var contexto= miCanvas.getContext("2d");
 contexto.fillStyle= "red";
 contexto.fillRect(30,30,50,50);
