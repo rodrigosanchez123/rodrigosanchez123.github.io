b2dc01a9fee7e264e12d66bfa7a147b91c468d81
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-21 19:36:47 -0800

    Update ejemplo2.21.1.js

--- ejemplo2.21/ejemplo2.21.1.js
+++ ejemplo2.21/ejemplo2.21.1.js
@@ -1,33 +1,37 @@
-var forma= new THREE.Geometry();
-forma.vertices.push( new THREE.Vector3(1,0,1)); //vertice 0
-forma.vertices.push( new THREE.Vector3(1,0,-1)); //vertice 1
-forma.vertices.push( new THREE.Vector3(-1,0,-1)); //vertice 2
-forma.vertices.push( new THREE.Vector3(-1,0,1)); // vertice 3
-forma.vertices.push( new THREE.Vector3(0,1,0)); //vertice 4
+var forma = new THREE.Geometry();
+forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 0
+forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
+forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
+forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
+forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
 
-forma.face.push( new THREE.Face3(3,2,1)); //cara 0
-forma.faces.push( new THREE.Face3(3,1,0)); //cara 1
-forma.faces.push( new THREE.Face3(3,0,4)); //cara 2
-forma.faces.push( new THREE.Face3(0,1,4)); //cara 3
-forma.faces.push( new THREE.Face3(1,2,4)); // cara 4
-forma.faces.push( new THREE.Face3(2,3,4)); //cara 5
+forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
+forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
+forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
+forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
+forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
+forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
 
 forma.computeBoundingSphere();
 
 forma.computeFaceNormals();
 
-var mterial= new THREE.MeshNormalMaterial();
+var material = new THREE.MeshNormalMaterial();
 
-var malla= new THREE.Mesh( forma, material);
+var malla = new THREE.Mesh( forma, material );
 malla.rotateX(Math.PI/4);
 
-var escena= new THREE.Scene();
-escena.add(malla);
 
-var camara= new THREE.PerspectiveCamara();
-camara.position.z= 5;
+var escena = new THREE.Scene();
+escena.add( malla );
 
-var renderizador= new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
-document.body.appendChild(renderizador.domElement);
-renderizador.render(escena, camara);
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, 
+                      window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
+
+
