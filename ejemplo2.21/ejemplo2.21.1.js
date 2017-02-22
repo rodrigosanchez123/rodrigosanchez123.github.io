59032a7888cd1feac777b5853f7f4abaf26b4669
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-21 19:29:10 -0800

    Update ejemplo2.21.1.js

--- ejemplo2.21/ejemplo2.21.1.js
+++ ejemplo2.21/ejemplo2.21.1.js
@@ -15,3 +15,19 @@ forma.faces.push( new THREE.Face3(2,3,4)); //cara 5
 forma.computeBoundingSphere();
 
 forma.computeFaceNormals();
+
+var mterial= new THREE.MeshNormalMaterial();
+
+var malla= new THREE.Mesh( forma, material);
+malla.rotateX(Math.PI/4);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+
+var camara= new THREE.PerspectiveCamara();
+camara.position.z= 5;
+
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
