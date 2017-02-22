ec95a2d778215b8e13650df5501862c7fe6b72fe
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-21 19:39:45 -0800

    Update ejemplo2.21.1.js

--- ejemplo2.21/ejemplo2.21.1.js
+++ ejemplo2.21/ejemplo2.21.1.js
@@ -5,12 +5,12 @@ forma.vertices.push( new THREE.Vector3(-1,0,-1)); // Vértice 2
 forma.vertices.push( new THREE.Vector3(-1,0,1)); // Vértice 3
 forma.vertices.push( new THREE.Vector3(0,1,0)); // Vértice 4
 
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
-forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
-forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
-forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
+forma.faces.push( new THREE.Face3(3,2,1)); // Cara 0
+forma.faces.push( new THREE.Face3(3,1,0)); // Cara 1
+forma.faces.push( new THREE.Face3(3,0,4)); // Cara 2
+forma.faces.push( new THREE.Face3(0,1,4)); // Cara 3
+forma.faces.push( new THREE.Face3(1,2,4)); // Cara 4
+forma.faces.push( new THREE.Face3(2,3,4)); // Cara 5
 
 forma.computeBoundingSphere();
 
