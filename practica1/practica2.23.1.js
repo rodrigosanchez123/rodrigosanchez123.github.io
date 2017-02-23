19568baf5ac581d3518015a374f9067e2591ca47
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-23 14:21:41 -0600

    Update practica2.23.1.js

--- practica1/practica2.23.1.js
+++ practica1/practica2.23.1.js
@@ -6,6 +6,8 @@ personaje.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // vertice 3
 personaje.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // vertice 4
 //personaje.vertices.push( new THREE.Vector3( 0, -1,  0 ) ); // vertice 5
 
+personaje.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
+personaje.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
 personaje.faces.push( new THREE.Face3( 3, 0, 4) );
 personaje.faces.push( new THREE.Face3( 0, 1, 4) );
 personaje.faces.push( new THREE.Face3( 1, 2, 4) );
