5cc5e3315122851bd1a1493e60d2afc0b7df8760
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-24 14:01:14 -0600

    Update ejemplo2.21.4.js

--- ejemplo2.21/ejemplo2.21.4.js
+++ ejemplo2.21/ejemplo2.21.4.js
@@ -8,9 +8,9 @@ figura.lineTo(10, 10);
 var forma = new THREE.ExtrudeGeometry( figura,
                                        {amount: 10} );
 
-var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh( forma, material );
-malla.rotateY( Math.PI/4 );
+var material= new THREE.MeshNormalMaterial();
+var malla= new THREE.Mesh( forma, material );
+malla.rotateY(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add(malla);
