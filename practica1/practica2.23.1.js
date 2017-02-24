a4e0dec3a14383f61dcbd60ae15e171ed3d43a99
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-23 21:30:56 -0800

    Update practica2.23.1.js

--- practica1/practica2.23.1.js
+++ practica1/practica2.23.1.js
@@ -22,7 +22,7 @@ var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( personaje, material );
 malla.rotateX(Math.PI/4);
-malla.rotateZ(Math.PI/4);
+malla.rotateZ(Math.PI/8);
 
 
 var escena = new THREE.Scene();
