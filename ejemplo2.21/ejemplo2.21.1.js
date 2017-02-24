f267bef83a0bca1983eafa82094fd8c7fc466a64
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-24 14:10:21 -0600

    Update ejemplo2.21.1.js

--- ejemplo2.21/ejemplo2.21.1.js
+++ ejemplo2.21/ejemplo2.21.1.js
@@ -1,5 +1,5 @@
 var forma = new THREE.Geometry();
-forma.vertices.push( new THREE.Vector3(0,0,1)); // Vértice 0
+forma.vertices.push( new THREE.Vector3(1,0,1)); // Vértice 0
 forma.vertices.push( new THREE.Vector3(1,0,-1)); // Vértice 1
 forma.vertices.push( new THREE.Vector3(-1,0,-1)); // Vértice 2
 forma.vertices.push( new THREE.Vector3(-1,0,1)); // Vértice 3
