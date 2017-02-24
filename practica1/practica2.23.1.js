3d83ed13649eeb76fd6a773a886ec087fce7412d
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-23 19:15:24 -0800

    Update practica2.23.1.js

--- practica1/practica2.23.1.js
+++ practica1/practica2.23.1.js
@@ -28,7 +28,7 @@ var escena = new THREE.Scene();
 escena.add( malla );
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.x = 5;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, 
