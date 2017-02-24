1b9419a852469f15564e5e736db88cd2a6fc5bf4
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-23 17:12:38 -0800

    Update practica2.23.1.js

--- practica1/practica2.23.1.js
+++ practica1/practica2.23.1.js
@@ -4,18 +4,18 @@ personaje.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // vertice 1
 personaje.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // vertice 2
 personaje.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // vertice 3
 personaje.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // vertice 4
-//personaje.vertices.push( new THREE.Vector3( 0, -1,  0 ) ); // vertice 5
+personaje.vertices.push( new THREE.Vector3( 0, -1,  0 ) ); // vertice 5
 
-personaje.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
-personaje.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
+//personaje.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
+//personaje.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
 personaje.faces.push( new THREE.Face3( 3, 0, 4 ) );
 personaje.faces.push( new THREE.Face3( 0, 1, 4 ) );
 personaje.faces.push( new THREE.Face3( 1, 2, 4 ) );
 personaje.faces.push( new THREE.Face3( 2, 3, 4 ) );
-//personaje.faces.push( new THREE.Face3( 3, 0, 5) );
-//personaje.faces.push( new THREE.Face3( 0, 1, 5) );
-//personaje.faces.push( new THREE.Face3( 1, 2, 5) );
-//personaje.faces.push( new THREE.Face3( 2, 3, 5) );
+personaje.faces.push( new THREE.Face3( 3, 0, 5) );
+personaje.faces.push( new THREE.Face3( 0, 1, 5) );
+personaje.faces.push( new THREE.Face3( 1, 2, 5) );
+personaje.faces.push( new THREE.Face3( 2, 3, 5) );
 
 personaje.computeBoundingSphere();
 personaje.computeFaceNormals();
