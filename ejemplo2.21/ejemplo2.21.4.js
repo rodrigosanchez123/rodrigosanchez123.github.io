cc60820b6c946cfd5004fa3c7a72ea01d8e9cf18
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-24 14:07:05 -0600

    Update ejemplo2.21.4.js

--- ejemplo2.21/ejemplo2.21.4.js
+++ ejemplo2.21/ejemplo2.21.4.js
@@ -1,6 +1,6 @@
 var figura = new THREE.Shape();
 
-figura.moveTo(10, 10);
+figura.moveTo(0, 10);
 figura.lineTo(10, 40);
 figura.lineTo(40, 40);
 figura.lineTo(10, 10);
@@ -16,7 +16,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 50;
+camara.position.z = 500;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
