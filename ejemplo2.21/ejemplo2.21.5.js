2ace0fe42890ac74c24c3c247e0a373030e396d3
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-02-28 13:43:04 -0600

    Update ejemplo2.21.5.js

--- ejemplo2.21/ejemplo2.21.5.js
+++ ejemplo2.21/ejemplo2.21.5.js
@@ -10,7 +10,7 @@ var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI/6 );
+//malla.rotateX( Math.PI/6 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
