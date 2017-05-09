62d61e0854a0e26ca95b931a28c05be40fbca20c
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-09 13:31:01 -0500

    Create Caja.js

---
+++ texturas/Ejemplo1/Caja.js
@@ -0,0 +1,29 @@
+function setup(){
+var textura = THREE.ImageUtils.loadTexture('crate.gif');
+var material = new THREE:MeshBasicMaterial({map: textura});
+var forma = new THREE.BoxGeometry(1,1,1);
+malla = new THREE.Mesh(forma,material);
+
+escena = new THREE.Scene();
+escena.add(malla);
+
+camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+requestAnimation(loop);
+
+malla.rotation.x += 0.01;
+malla.rotation.y += 0.01;
+
+renderer.render(escena,camara);
+}
+
+var camara, escena, renderer, malla;
+setup();
+loop();
