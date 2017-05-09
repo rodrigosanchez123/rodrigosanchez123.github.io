9613f3f9359c8118448d40f04e9622f3463f4746
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-09 13:47:54 -0500

    Update Caja.js

--- texturas/Ejemplo1/Caja.js
+++ texturas/Ejemplo1/Caja.js
@@ -1,7 +1,7 @@
 function setup(){
 THREE.ImageUtils.crossOrigin = '';
 var textura = THREE.ImageUtils.loadTexture('https:crate.gif');
-var material = new THREE:MeshBasicMaterial({map: textura});
+var material = new THREE.MeshBasicMaterial({map: textura});
 var forma = new THREE.BoxGeometry(1,1,1);
 malla = new THREE.Mesh(forma,material);
 
