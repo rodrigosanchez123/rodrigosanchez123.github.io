2ea90b1898d367b06f5f72d78d5356f099941aba
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-09 14:07:08 -0500

    Update Ladrillo.js

--- texturas/Ejemplo2/Ladrillo.js
+++ texturas/Ejemplo2/Ladrillo.js
@@ -1,5 +1,6 @@
 function setup(){
 THREE.ImageUtils.crossOrigin = '';
-var textura = THREE.ImageUtils.loadTexture('');
+var textura = THREE.ImageUtils.loadTexture('https:create.js');
 var material = new THREE.MeshLambertMaterial({map: textura});
-
+var forma = new THREE.BoxGeometry(1,4,9);
+malla = new THREE.Mesh();
