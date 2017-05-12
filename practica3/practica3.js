3945ce0f2af71f47a9bb21bd74cd43734e0b4214
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-11 19:44:04 -0700

    Update practica3.js

--- practica3/practica3.js
+++ practica3/practica3.js
@@ -4,7 +4,7 @@ function setup(){
   var PlanoMaterial = new THREE.MeshBasicMaterial({map: planoimagen});
   var PlanoGeometria = new THREE.PlaneGeometry(50,70);
   plano = new THREE.Mesh(PlanoGeometria,PlanoMaterial);
-  //plano.rotateX(Math.PI/-4);
+  plano.rotation.x=10;
 
   escena = new THREE.Scene();
   escena.add(plano);
@@ -15,18 +15,18 @@ function setup(){
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth,window.innerHeight);
   document.body.appendChild(renderer.domElement);
-  //renderer.render(escena,camara);
+  renderer.render(escena,camara);
 }
 
-function loop(){
-requestAnimationFrame(loop);
+//function loop(){
+//requestAnimationFrame(loop);
 
-plano.rotation.x += 0.01;
+//plano.rotation.x += 0.01;
 //plano.rotation.y += 0.01;
 
-renderer.render(escena,camara);
-}
+//renderer.render(escena,camara);
+//}
 
 var plano, escena, camara, renderer;
 setup();
-loop();
+//loop();
