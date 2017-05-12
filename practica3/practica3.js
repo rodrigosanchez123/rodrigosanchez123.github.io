56bf4fc91ac1fce4b2cea2e1cbc1fa63ea0a5705
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-11 18:55:30 -0700

    Update practica3.js

--- practica3/practica3.js
+++ practica3/practica3.js
@@ -4,7 +4,7 @@ function setup(){
   var PlanoMaterial = new THREE.MeshBasicMaterial({map: planoimagen});
   var PlanoGeometria = new THREE.PlaneGeometry(50,70);
   plano = new THREE.Mesh(PlanoGeometria,PlanoMaterial);
-  plano.rotateX(Math.PI/-2);
+  plano.rotateX(Math.PI/4);
 
   escena = new THREE.Scene();
   escena.add(plano);
