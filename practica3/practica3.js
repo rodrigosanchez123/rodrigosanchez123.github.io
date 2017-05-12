2aa1054c03c9f99065a5eb8aca15b3d91b18d7b6
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-11 18:37:50 -0700

    Update practica3.js

--- practica3/practica3.js
+++ practica3/practica3.js
@@ -20,8 +20,8 @@ function setup(){
 function loop(){
 requestAnimationFrame(loop);
 
-malla.rotation.x += 0.01;
-malla.rotation.y += 0.01;
+plano.rotation.x += 0.01;
+plano.rotation.y += 0.01;
 
 renderer.render(escena,camara);
 }
