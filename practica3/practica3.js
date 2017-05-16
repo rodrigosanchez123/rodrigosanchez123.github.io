940fec98746342e6cc06fdb285279a09770f1bcb
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-15 17:25:46 -0700

    Update practica3.js

--- practica3/practica3.js
+++ practica3/practica3.js
@@ -1,9 +1,7 @@
 function setup(){
-  //THREE.ImageUtils.crossOrigin = '';
-  //var planoimagen = new THREE.ImageUtils.loadTexture('https://rodrigosanchez123.github.io/practica3/Cragmaw.jpg');
-  var texloader = new THREE.TextureLoader();
-  var tex=texloader.load("Cragmaw.jpg");
-  var PlanoMaterial = new THREE.MeshBasicMaterial({map:tex, side:THREE.DoubleSide});
+  THREE.ImageUtils.crossOrigin = '';
+  var planoimagen = THREE.ImageUtils.loadTexture('https://rodrigosanchez123.github.io/practica3/Cragmaw.jpg');
+  var PlanoMaterial = new THREE.MeshBasicMaterial({map:planoimagen, side:THREE.DoubleSide});
   var PlanoGeometria = new THREE.PlaneGeometry(50,70);
   plano = new THREE.Mesh(PlanoGeometria,PlanoMaterial);
   plano.rotateX(Math.PI/-4);
