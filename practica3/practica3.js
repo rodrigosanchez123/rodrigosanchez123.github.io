f5923e6ac7db6ac3f4b433d5f2eac62c97083137
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-25 20:29:20 -0700

    Update practica3.js

--- practica3/practica3.js
+++ practica3/practica3.js
@@ -1,6 +1,6 @@
 function setup(){
   THREE.ImageUtils.crossOrigin = '';
-  var planoimagen = THREE.ImageUtils.loadTexture('https:Cragmaw.jpg');
+  var planoimagen = THREE.ImageUtils.loadTexture('https:Cragmaw.jpg',{},function(){renderer.render(escena);});
   var PlanoMaterial = new THREE.MeshBasicMaterial({map:planoimagen, side:THREE.DoubleSide});
   var PlanoGeometria = new THREE.BoxGeometry(50,70,10);
   plano = new THREE.Mesh(PlanoGeometria,PlanoMaterial);
@@ -16,18 +16,17 @@ function setup(){
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth,window.innerHeight);
   document.body.appendChild(renderer.domElement);
- 
+  renderer.render(escena,camara);
 }
 
-function loop(){
-requestAnimationFrame(loop);
+//function loop(){
+//requestAnimationFrame(loop);
 
 //plano.rotation.x += 0.01;
 //plano.rotation.y += 0.01;
 
 //renderer.render(escena,camara);
- renderer.render(escena,camara);
-}
+//}
 
 var plano, escena, camara, renderer;
 setup();
