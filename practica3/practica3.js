793c08e2d2fc9d2b515f4171467370599586444f
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-05-30 14:20:48 -0500

    Update practica3.js

--- practica3/practica3.js
+++ practica3/practica3.js
@@ -1,13 +1,16 @@
 function setup(){ 
   THREE.ImageUtils.crossOrigin = '';
-  var planoimagen = THREE.ImageUtils.loadTexture('https:Cragmaw.jpg');
-  var PlanoMaterial = new THREE.MeshBasicMaterial({map:planoimagen, side:THREE.DoubleSide});
-  var PlanoGeometria = new THREE.BoxGeometry(50,70,10);
+  //var planoimagen = THREE.ImageUtils.loadTexture('https:Cragmaw.jpg');
+  
+ escena = new THREE.Scene();
+  var loader= new THREE.TextureLoader();
+     loader.load('https:Cragmaw.jpg', function ( planoimagen ) {
+    var PlanoMaterial = new THREE.MeshBasicMaterial({map:planoimagen, side:THREE.DoubleSide});
+    var PlanoGeometria = new THREE.BoxGeometry(50,70,10);
   plano = new THREE.Mesh(PlanoGeometria,PlanoMaterial);
   plano.rotateX(Math.PI/-4);
-  
-  escena = new THREE.Scene();
-  escena.add(plano);
+       escena.add(plano);
+     });
   
   camara = new THREE.PerspectiveCamera();
   camara.position.z = 50;
@@ -16,18 +19,21 @@ function setup(){
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth,window.innerHeight);
   document.body.appendChild(renderer.domElement);
+  renderer.render(escena,camara);
  
 }
 
-function loop(){
-requestAnimationFrame(loop);
+//function loop(){
+//requestAnimationFrame(loop);
 
 //plano.rotation.x += 0.01;
 //plano.rotation.y += 0.01;
 
-renderer.render(escena,camara);
-}
+//renderer.render(escena,camara);
+//}
+
+
 
 var plano, escena, camara, renderer;
 setup();
-loop();
+//loop();
