2e248e732ef4aa64d6ecaf191e689ab5567e93d8
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-06-08 19:47:26 -0700

    Create RayCaster2.js

--- colision/RayCaster2.js
+++ colision/RayCaster2.js
@@ -9,8 +9,8 @@ cubo2.position.x = -7;
 camara = new THREE.PerspectiveCamera();
 camara.position.z = 20;
 
-raycaster1 = new THREE.RayCaster(pelota.position, new THREE.Vector3(1,0,0));
-raycaster2 = new THREE.RayCaster(pelota.position, new THREE.vector3(-1,0,0));
+raycaster1 = new THREE.Raycaster(pelota.position, new THREE.Vector3(1,0,0));
+raycaster2 = new THREE.Raycaster(pelota.position, new THREE.vector3(-1,0,0));
 
 escena = new THREE.Scene();
 escena.add(cubo1);
