28cb5b870e3302ac891afb01a220a9bab395bf72
rodrigosanchez123 <smallville-1994@live.com.mx>
2017-06-11 20:15:00 -0700

    Create Juego.js

--- Proyecto/Juego.js
+++ Proyecto/Juego.js
@@ -82,12 +82,12 @@ var alien = new THREE.JSONLoader();
      alien.load('https:Personajes/XenoRanger.json',function ( geometry, materials ) {
      	var material4 = materials[0];
 	var personaje4 = new THREE.Mesh( geometry, material4 );
-	personaje4.position.set(-10,0,30);
+	personaje4.position.set(-10,0,90);
 	personaje4.scale.set(4,4,4);
       	escena.add( personaje4 );	});	
 	
 var amber = new THREE.JSONLoader();
-     amber.load('https:Personajes/Ember.json',function ( geometry, materials ) {
+     amber.load('https:Personajes/Lizard.json',function ( geometry, materials ) {
      	var material5 = materials[0];
 	var personaje5 = new THREE.Mesh( geometry, material5 );
 	personaje5.position.set(-50,0,80);
